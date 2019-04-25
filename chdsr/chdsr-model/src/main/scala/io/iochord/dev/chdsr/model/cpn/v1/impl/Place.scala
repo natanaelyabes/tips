2a25eb949a199212b20a57dd1a8bf33b58ab66ff
nur ichsan utama <ichsan83@gmail.com>
2019-04-25 15:35:36 +0900

    * add get method in model (set only from construction)

--- chdsr/chdsr-model/src/main/scala/io/iochord/dev/chdsr/model/cpn/v1/impl/Place.scala
+++ chdsr/chdsr-model/src/main/scala/io/iochord/dev/chdsr/model/cpn/v1/impl/Place.scala
@@ -3,11 +3,13 @@ package io.iochord.dev.chdsr.model.cpn.v1.impl
 import io.iochord.dev.chdsr.model.cpn.v1.Element
 import io.iochord.dev.chdsr.model.cpn.v1.Node
 
+import scala.collection.generic._
+
 case class Place[T] (
   id: String,
   name: String,
   initialMarking: T) extends Element with Node {
-
+  
   var currentMarking = initialMarking
   
   def initState() = currentMarking = initialMarking
@@ -19,11 +21,28 @@ case class Place[T] (
   def addTokens(tokens: Any) = { }
   
   var in = List[Arc[_]]()
+  
   var out = List[Arc[_]]()
+  
   def addIn(arc: Arc[_]) {
     in = arc :: in
   }
+  
   def addOut(arc: Arc[_]) {
     out = arc :: out
   }
+  
+  def getcurrentMarking():T = currentMarking
+  
+  def setCurrentMarking(cm:T) {
+    currentMarking = cm
+  }
+  
+  def getIn(): List[Arc[_]] = in
+  
+  def getOut():List[Arc[_]] = out
+  
+  def getId():String = id
+  
+  def getName():String = name
 }
