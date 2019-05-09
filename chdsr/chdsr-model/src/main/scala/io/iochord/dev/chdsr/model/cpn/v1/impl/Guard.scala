1e7f3e9bc6aa8512c81fa3b0b852eef4babe1e51
nur ichsan utama <ichsan83@gmail.com>
2019-05-09 15:24:22 +0900

    * Update class Arc.scala change arcinscription function (separate between setarcinsc and input) * Update class Guard.scala change guardexpression function (separate between setguardexpression and input) * Update class Transition.scala, adding isCheckEnabled and isArcEnabled function * Update class Simulator.scala, enabled checking for transition already added, simulator can be run to check if the there is any transition enabled based on the current marking + Adding Test class to run the simulation with simple graph structure with 2 input places, 1 transition, 2 arcs (with guard and arc inscription) SimulationTestRunWithBinding.scala

--- chdsr/chdsr-model/src/main/scala/io/iochord/dev/chdsr/model/cpn/v1/impl/Guard.scala
+++ chdsr/chdsr-model/src/main/scala/io/iochord/dev/chdsr/model/cpn/v1/impl/Guard.scala
@@ -4,13 +4,17 @@ import io.iochord.dev.chdsr.model.cpn.v1._
 
 class Guard { 
   
+  var guardBind:List[Any] => Boolean = null
+  
   def cond1[T <: Bind](cond:T => Boolean, inp:T):Boolean = {
     cond(inp)
   }
   
-  /*
-  def cond2(cond:List[Any] => Boolean, inp:Any):Boolean = {
-    cond(inp)
+  def setGuardBind(eval: List[Any] => Boolean) {
+    guardBind = eval
   }
-  */
+  
+  def evalGuard(inp:List[Any]):Boolean = {
+    guardBind(inp)
+  }
 }
