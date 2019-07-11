3e4acc6c30a9acf03a513a427cc19c420c861a07
Taufik Nur Adi <taufik.nur.adi@gmail.com>
2019-07-11 11:00:12 +0900

    + Completing ActivityNode Prop in Parent/Child

--- chdsr/chdsr-simulator-web/src/main/ts/src/iochord/chdsr/simulation/editor/components/canvas/components/CanvasComponent.vue
+++ chdsr/chdsr-simulator-web/src/main/ts/src/iochord/chdsr/simulation/editor/components/canvas/components/CanvasComponent.vue
@@ -36,7 +36,32 @@
       </template>
 
       <template v-if="type === 'activity'">
-        <ActivityNodeModal v-bind:id="type" v-bind:key="type"/>
+        <ActivityNodeModal
+        @changeActNodeSelectedActivityType = "changeActNodeSelectedActivityTypeFromChild($event)"
+        @changeActNodeReport = "changeActNodeReportFromChild($event)"
+        @changeActNodeCustomMonitor = "changeActNodeCustomMonitorFromChild($event)"
+        @changeActNodeProcessingTime = "changeActNodeProcessingTimeFromChild($event)"
+        @changeActNodeParameter1 = "changeActNodeParameter1FromChild($event)"
+        @changeActNodeSetupTime = "changeActNodeSetupTimeFromChild($event)"
+        @changeActNodeParameter2 = "changeActNodeParameter2FromChild($event)"
+        @changeActNodeUnit = "changeActNodeUnitFromChild($event)"
+        @changeActNodeQueueLabel = "changeActNodeQueueLabelFromChild($event)"
+        @changeActNodeInputType = "changeActNodeInputTypeFromChild($event)"
+        @changeActNodeOutputType = "changeActNodeOutputTypeFromChild($event)"
+        @changeActNodeCodeSegment = "changeActNodeCodeSegmentFromChild($event)"
+        :actNodeSelectedActivityType = "parentActNodeSelectedActivityType"
+        :actNodeReport = "parentActNodeReport"
+        :actNodeCustomMonitor = "parentActNodeCustomMonitor"
+        :actNodeProcessingTime = "parentActNodeProcessingTime"
+        :actNodeParameter1 = "parentActNodeParameter1"
+        :actNodeSetupTime = "parentActNodeSetupTime"
+        :actNodeParameter2 = "parentActNodeParameter2"
+        :actNodeUnit = "parentActNodeUnit"
+        :actNodeQueueLabel = "parentActNodeQueueLabel"
+        :actNodeInputType = "parentActNodeInputType"
+        :actNodeOutputType = "parentActNodeOutputType"
+        :actNodeCodeSegment = "parentActNodeCodeSegment"
+        v-bind:id="type" v-bind:key="type"/>
       </template>
 
       <template v-if="type === 'stop'">
@@ -143,6 +168,19 @@ export default class CanvasComponent extends BaseComponent {
   public parentBranchSelectedType: string = '';
   public parentBranchSelectedRule: string = '';
 
+  public parentActNodeSelectedActivityType: string = '';
+  public parentActNodeReport: boolean = false;
+  public parentActNodeCustomMonitor: string = '';
+  public parentActNodeProcessingTime: string = '';
+  public parentActNodeParameter1: string = '';
+  public parentActNodeSetupTime: string = '';
+  public parentActNodeParameter2: string = '';
+  public parentActNodeUnit: string = '';
+  public parentActNodeQueueLabel: string = '';
+  public parentActNodeInputType: string = '';
+  public parentActNodeOutputType: string = '';
+  public parentActNodeCodeSegment: string = '';
+
   public parentStopLabel: string = '';
   public parentStopReport: boolean = false;
 
@@ -185,7 +223,7 @@ export default class CanvasComponent extends BaseComponent {
     this.parentBranchSelectedType = newVal;
   }
 
-  public changeBranchSelectedRule(newVal: string) {
+  public changeBranchSelectedRule(newVal: string): void {
     this.parentBranchSelectedRule = newVal;
   }
 
@@ -204,6 +242,107 @@ export default class CanvasComponent extends BaseComponent {
 
   public changeBranchSelectedRuleFromChild(e: any) {
     this.parentBranchSelectedRule = e;
+  }
+
+/*
+  Activity Node functions
+  - Send changes from parent to child
+  - Retrieve changes from child to parent
+*/
+
+  public changeActNodeSelectedActivityType(newVal: string): void {
+    this.parentActNodeSelectedActivityType = newVal;
+  }
+
+  public changeActNodeReport(newVal: boolean): void {
+    this.parentActNodeReport = newVal;
+  }
+
+  public changeActNodeCustomMonitor(newVal: string): void {
+    this.parentActNodeCustomMonitor = newVal;
+  }
+
+  public changeActNodeProcessingTime(newVal: string): void {
+    this.parentActNodeSelectedActivityType = newVal;
+  }
+
+  public changeActNodeParameter1(newVal: string): void {
+    this.parentActNodeParameter1 = newVal;
+  }
+
+  public changeActNodeSetupTime(newVal: string): void {
+    this.parentActNodeSetupTime = newVal;
+  }
+
+  public changeActNodeParameter2(newVal: string): void {
+    this.parentActNodeParameter2 = newVal;
+  }
+
+  public changeActNodeChangeUnit(newVal: string): void {
+    this.parentActNodeUnit = newVal;
+  }
+
+  public changeActNodeQueueLabel(newVal: string): void {
+    this.parentActNodeQueueLabel = newVal;
+  }
+
+  public changeActNodeInputType(newVal: string): void {
+    this.parentActNodeInputType = newVal;
+  }
+
+  public changeActNodeOutputType(newVal: string): void {
+    this.parentActNodeOutputType = newVal;
+  }
+
+  public changeActNodeCodeSegment(newVal: string): void {
+    this.parentActNodeCodeSegment = newVal;
+  }
+
+/* From Child */
+  public changeActNodeSelectedActivityTypeFromChild(e: any) {
+    this.parentActNodeSelectedActivityType = e;
+  }
+
+  public changeActNodeReportFromChild(e: any) {
+    this.parentActNodeReport = e;
+  }
+
+  public changeActNodeCustomMonitorFromChild(e: any) {
+    this.parentActNodeCustomMonitor = e;
+  }
+
+  public changeActNodeProcessingTimeFromChild(e: any) {
+    this.parentActNodeProcessingTime = e;
+  }
+
+  public changeActNodeParameter1FromChild(e: any) {
+    this.parentActNodeParameter1 = e;
+  }
+
+  public changeActNodeSetupTimeFromChild(e: any) {
+    this.parentActNodeSetupTime = e;
+  }
+
+  public changeActNodeParameter2FromChild(e: any) {
+    this.parentActNodeParameter2 = e;
+  }
+
+  public changeActNodeUnitFromChild(e: any) {
+    this.parentActNodeUnit = e;
+  }
+
+  public changeActNodeQueueLabelFromChild(e: any) {
+    this.parentActNodeQueueLabel = e;
+  }
+
+  public changeActNodeInputTypeFromChild(e: any) {
+    this.parentActNodeInputType = e;
+  }
+  public changeActNodeOutputTypeFromChild(e: any) {
+    this.parentActNodeOutputType = e;
+  }
+  public changeActNodeCodeSegmentFromChild(e: any) {
+    this.parentActNodeCodeSegment = e;
   }
 
 /*
