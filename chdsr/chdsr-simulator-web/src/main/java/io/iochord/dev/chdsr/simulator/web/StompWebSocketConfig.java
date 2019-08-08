4cb75a6e2861033a8c360c4b60e9f08d61edaac1
Iq Reviessay Pulshashi <pulshashi@ideas.web.id>
2019-08-08 15:28:15 +0900

    + Web Service Mockup

---
+++ chdsr/chdsr-simulator-web/src/main/java/io/iochord/dev/chdsr/simulator/web/StompWebSocketConfig.java
@@ -0,0 +1,23 @@
+package io.iochord.dev.chdsr.simulator.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@Configuration
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-chdsr").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/req");
+        registry.enableSimpleBroker("/res");
+    }
+}
