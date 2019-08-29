f13835d9106465e500d276712174e53fa8d330a2
netanyahuyasser <yabes.wirawan@gmail.com>
2019-08-29 14:57:09 +0900

    * annotate several codes with comments so it can be refractor easily later

--- chdsr/chdsr-simulator-web/src/main/ts/src/iochord/chdsr/simulation/editor/components/canvas/CanvasComponent.vue
+++ chdsr/chdsr-simulator-web/src/main/ts/src/iochord/chdsr/simulation/editor/components/canvas/CanvasComponent.vue
@@ -7,7 +7,7 @@
   <div class="canvas component">
     <div class="editor canvas">
 
-      <!-- TODO: Develop mouse event handler for canvas -->
+      <!-- Canvas to render graph -->
       <div id="canvas"
         @keydown.esc="handleEscapeButton($event)"
         @mousedown="handleCanvasMouseDown($event)"
@@ -17,7 +17,11 @@
 
     <!-- Node Modals -->
     <template v-for="type in Array.from(nodeTypes)">
+
+      <!-- If start node was clicked -->
       <template v-if="type === 'start'">
+
+        <!-- Show start modal -->
         <StartNodeModal label="test"
           @changeStartLabel = "changeStartLabelFromChild($event, activePage, currentSelectedElement, loadGraph)"
           @changeStartGenerator = "changeStartGeneratorFromChild($event, activePage, currentSelectedElement, loadGraph)"
@@ -26,7 +30,10 @@
           v-bind:id="type" v-bind:key="type" />
       </template>
 
+      <!-- If branch node was clicked -->
       <template v-if="type === 'branch'">
+
+        <!-- Show branch modal -->
         <BranchNodeModal label="this is branch"
           @changeBranchLabel = "changeBranchLabelFromChild($event, activePage, currentSelectedElement, loadGraph)"
           @changeBranchSelectedGate = "changeBranchSelectedGateFromChild($event, activePage, currentSelectedElement, loadGraph)"
@@ -39,7 +46,10 @@
           v-bind:id="type" v-bind:key="type" />
       </template>
 
+      <!-- If activity node was clicked -->
       <template v-if="type === 'activity'">
+
+        <!-- Show activity modal -->
         <ActivityNodeModal
           @changeActNodeSelectedActivityType = "changeActNodeSelectedActivityTypeFromChild($event, activePage, currentSelectedElement, loadGraph)"
           @changeActNodeReport = "changeActNodeReportFromChild($event, activePage, currentSelectedElement, loadGraph)"
@@ -68,7 +78,10 @@
           v-bind:id="type" v-bind:key="type"/>
       </template>
 
+      <!-- If stop node was clicked -->
       <template v-if="type === 'stop'">
+
+        <!-- Show stop modal -->
         <StopNodeModal
           @changeStopLabel = "changeStopLabelFromChild($event, activePage, currentSelectedElement, loadGraph)"
           @changeStopReport = "changeStopReportFromChild($event, activePage, currentSelectedElement, loadGraph)"
@@ -225,8 +238,14 @@ export default class CanvasComponent extends Mixins(BaseComponent, ModalMixin, C
 
     // Helper to reset all elements
     const resetAll = (paper: joint.dia.Paper) => {
+
+      // Get all elements within graph
       jointPage.getGraph().getElements().forEach((element: joint.dia.Element) => {
+
+        // For each element, find its elementView
         const elementView = paper.findViewByModel(element);
+
+        // Unhighlight the element from the elementView
         elementView.unhighlight();
       });
     };
@@ -236,18 +255,20 @@ export default class CanvasComponent extends Mixins(BaseComponent, ModalMixin, C
       return (e: KeyboardEvent) => {
         const links = jointPage.getGraph().getConnectedLinks(currentElement);
 
+        // If delete key was pressed
         if (e.keyCode === 46) {
 
           // Remove node
           jointPage.getNodes()!.delete(currentElement.attributes.nodeId);
           currentElement.remove();
 
-          // Remove link
+          // Remove links that connected to the node as well
           links.forEach((link) => {
             jointPage.getArcs()!.delete(link.attributes.arcId);
             link.remove();
           });
 
+          // Remove event listener from the deleted nodes
           currentElement.off('keydown', removeNode(currentElement), false);
         }
       };
@@ -324,37 +345,58 @@ export default class CanvasComponent extends Mixins(BaseComponent, ModalMixin, C
         }
       },
       'element:mouseout': (elementView: joint.dia.ElementView) => {
+
+        // If currently drawing arc
         if (editorState.drawing) {
+
+          // Reset page
           resetAll(jointPage.getPaper());
+
+          // Set cursor to crosshair
           document.body.style.cursor = 'crosshair';
         } else {
+
+          // Otherwise set cursor to default
           document.body.style.cursor = 'default';
         }
       },
       'element:pointerdown': (elementView: joint.dia.ElementView) => {
+
+        // Reset page
         resetAll(jointPage.getPaper());
+
+        // Highlight current element
         const currentElement = elementView.model;
         currentElement.findView(jointPage.getPaper()).highlight();
 
         // When drawing a connector
         if (editorState.drawing) {
 
-          // User cannot drag anything when drawing a connector
+          // User cannot drag anything
           jointPage.getPaper().setInteractivity(false);
 
           // If source node is not set
           if (!this.source) {
+
+            // Set source node
             this.setSourceNode(this.activePage as JointGraphPageImpl, currentElement);
           } else if (this.source && !this.target) {
+
+            // Otherwise set it as target node
             this.setTargetNode(this.activePage as JointGraphPageImpl, currentElement);
           }
-
         } else {
+
+          // Otherwise set paper interactivity to true (user can drag everything)
           jointPage.getPaper().setInteractivity(true);
         }
       },
       'element:pointerclick': (elementView: joint.dia.ElementView) => {
+
+        // Reset page
         resetAll(jointPage.getPaper());
+
+        // Highlight current element
         const currentElement = elementView.model;
         currentElement.findView(jointPage.getPaper()).highlight();
       },
@@ -363,14 +405,21 @@ export default class CanvasComponent extends Mixins(BaseComponent, ModalMixin, C
         // Reset page
         resetAll(jointPage.getPaper());
 
+        // Get current element and its properties
         const currentElement = elementView.model;
         const currentElementType = currentElement.attributes.type;
         const currentElementNodeId = currentElement.attributes.nodeId;
 
+        // Highlight current element
         currentElement.findView(jointPage.getPaper()).highlight();
 
+        // If current clicked element is a start node
         if (currentElementType === 'start') {
+
+          // Show start modal
           $('#start').modal('show');
+
+          // Populate node properties to the modal
           this.parentStartLabel = jointPage.getNodes()!.get(currentElementNodeId)!.getLabel() as string;
           this.parentStartGenerator =
             (jointPage.getNodes()!.get(currentElementNodeId)! as GraphStartEventNode)
@@ -378,30 +427,51 @@ export default class CanvasComponent extends Mixins(BaseComponent, ModalMixin, C
               ' - ' +
             (jointPage.getNodes()!.get(currentElementNodeId)! as GraphStartEventNode)
               .getGenerator()!.getDistributionType() as string;
+
+          // Set current clicked node as current selected element
           this.currentSelectedElement = jointPage.getNodes()!.get(currentElementNodeId);
         }
 
+        // If current clicked element is an activity node
         if (currentElementType === 'activity') {
+
+          // Show activity modal
           $('#activity').modal('show');
         }
 
+        // If current clicked element is a branch node
         if (currentElementType === 'branch') {
+
+          // Show branch modal
           ($('#branch') as any).modal('show');
         }
 
+        // If current clicked element is a stop node
         if (currentElementType === 'stop') {
+
+          // Show stop modal
           $('#stop').modal('show');
+
+          // Populate node properties to the modal
           this.parentStopLabel = jointPage.getNodes()!.get(currentElementNodeId)!.getLabel() as string;
           this.parentStopReport = jointPage.getNodes()!.get(currentElementNodeId)!.isReportStatistics() as boolean;
+
+          // Set current clicked node as current selected element
           this.currentSelectedElement = jointPage.getNodes()!.get(currentElementNodeId);
         }
       },
       'cell:highlight': (elementView: joint.dia.ElementView) => {
+
+        // Get current element
         const currentElement = elementView.model;
+
+        // If delete key was pressed, remove node
         currentElement.on('keydown', removeNode(currentElement), false);
       },
       'cell:unhighlight': (elementView: joint.dia.ElementView) => {
         const currentElement = elementView.model;
+
+        // When cell is unhighlighted, remove the event listener for removing nodes
         currentElement.off('keydown', removeNode(currentElement), false);
       },
     });
